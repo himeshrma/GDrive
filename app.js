1691f43d1d49f0c764caf6fe0542b2de6dad12b5
himeshrma <sharmahimesh2003@gmail.com>
2025-10-02 18:08:48 +0530

    nEW sTRUCTURE

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 const userRoutes = require("./routes/user.routes");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 app.set("view engine", "ejs");
 
-app.use("/users", userRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", userRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 router.get("/test", (req, res) => {
   res.send("This is a test route");
@@ -9,4 +10,23 @@ router.get("/register", (req, res) => {
   res.render("register");
 });
 
+router.post(
+  "/register",
+  body("email").trim().isEmail().isLength({ min: 5 }),
+  body("password").trim().isLength({ min: 6 }),
+  body("username").trim().notEmpty(),
+  (req, res) => {
+    const errors = validationResult(req);
+
+    console.log(req.body);
+
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ errors: errors.array(), message: "Invalid Data!" });
+    }
+
+    res.send("Registration successful!");
+  }
+);
 module.exports = router;
